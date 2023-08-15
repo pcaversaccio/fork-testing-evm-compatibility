f21ad5d481183502ca5691fe4ab9348ab56981c7
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-08-15 10:52:52 +0200

    🔥 PoC is working
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- README.md
+++ README.md
@@ -1,66 +1,11 @@
-## Foundry
+# Fork Testing – EVM Compatibility
 
-**Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
-
-Foundry consists of:
-
--   **Forge**: Ethereum testing framework (like Truffle, Hardhat and DappTools).
--   **Cast**: Swiss army knife for interacting with EVM smart contracts, sending transactions and getting chain data.
--   **Anvil**: Local Ethereum node, akin to Ganache, Hardhat Network.
--   **Chisel**: Fast, utilitarian, and verbose solidity REPL.
-
-## Documentation
-
-https://book.getfoundry.sh/
-
-## Usage
-
-### Build
-
-```shell
-$ forge build
-```
+This repository implements a simple fork test on Optimism that proves that the EVM behaviour of the forked chain is _not_ identically replicated locally. This can lead to problems due to new opcodes such as [`PUSH0`](https://www.evm.codes/#5f?fork=shanghai) which would lead to untested behaviour as the EVM behaviour is not correctly replicated as part of the test suite. The underlying issue is that fork tests are local simulations with accounts, states, and storage slot information fetched from the RPC. This means that one does not get the full behaviour of the chain and therefore cannot identify specific EVM difference problems.
 
 ### Test
 
-```shell
-$ forge test
-```
+Simply invoke:
 
-### Format
-
-```shell
-$ forge fmt
-```
-
-### Gas Snapshots
-
-```shell
-$ forge snapshot
-```
-
-### Anvil
-
-```shell
-$ anvil
-```
-
-### Deploy
-
-```shell
-$ forge script script/Counter.s.sol:CounterScript --rpc-url <your_rpc_url> --private-key <your_private_key>
-```
-
-### Cast
-
-```shell
-$ cast <subcommand>
-```
-
-### Help
-
-```shell
-$ forge --help
-$ anvil --help
-$ cast --help
+```console
+forge test
 ```
---
+++ test/Store.t.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: WTFPL
+pragma solidity 0.8.21;
+
+import {Test, console2} from "forge-std/Test.sol";
+
+contract Store {
+    uint256 private _number;
+
+    function store(uint256 num) public {
+        _number = num;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return _number;
+    }
+}
+
+contract StoreTest is Test {
+    uint256 private optimismFork;
+    Store private store;
+
+    function setUp() public {
+        optimismFork = vm.createFork("https://mainnet.optimism.io");
+    }
+
+    function testDeployAndInteractOnOptimism() public {
+        // We activate the Optimism fork and assert it accordingly.
+        vm.selectFork(optimismFork);
+        assertEq(vm.activeFork(), optimismFork);
+
+        // This deployment should fail due to the non-existent `PUSH0` (`5f`) opcode in the init code.
+        store = new Store();
+        // 0x6080604052348015600e575f80fd5b5060a58061001b5f395ff3fe6080604052348015600e575f80fd5b50600436106030575f3560e01c80632e64cec11460345780636057361d146048575b5f80fd5b5f5460405190815260200160405180910390f35b605760533660046059565b5f55565b005b5f602082840312156068575f80fd5b503591905056fea264697066735822122066c9c950dae1553a207fada55ceb3c8c4a2cbd99fa68605393a2cae9c2dab02364736f6c63430008150033
+        console2.logBytes(type(Store).creationCode);
+
+        // These function interactions should fail due to the non-existent `PUSH0` (`5f`) opcode in the runtime code.
+        store.store(1);
+        assertEq(store.retrieve(), 1);
+        // 0x6080604052348015600e575f80fd5b50600436106030575f3560e01c80632e64cec11460345780636057361d146048575b5f80fd5b5f5460405190815260200160405180910390f35b605760533660046059565b5f55565b005b5f602082840312156068575f80fd5b503591905056fea2646970667358221220a20cff41929e97651791862ac5880f8582f7ae69d846a86b70433f4db10c6a7464736f6c63430008150033
+        console2.logBytes(type(Store).runtimeCode);
+    }
+}
