b99a1eb2207dd7099b6693ebceab749253814dc8
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-08-15 11:58:58 +0200

    ♻️ cleanup
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- test/Store.t.sol
+++ test/Store.t.sol
@@ -15,27 +15,30 @@ contract Store {
     }
 }
 
+/**
+ * @dev In the `foundry.toml` file we have configured the target EVM to be `shanghai`.
+ */
 contract StoreTest is Test {
-    uint256 private optimismFork;
-    Store private store;
+    uint256 private _optimismFork;
+    Store private _store;
 
     function setUp() public {
-        optimismFork = vm.createFork("https://mainnet.optimism.io");
+        _optimismFork = vm.createFork("https://mainnet.optimism.io");
     }
 
     function testDeployAndInteractOnOptimism() public {
         // We activate the Optimism fork and assert it accordingly.
-        vm.selectFork(optimismFork);
-        assertEq(vm.activeFork(), optimismFork);
+        vm.selectFork(_optimismFork);
+        assertEq(vm.activeFork(), _optimismFork);
 
-        // This deployment should fail due to the non-existent `PUSH0` (`5f`) opcode in the init code.
-        store = new Store();
+        // This deployment should actually fail due to the non-existent `PUSH0` (`5f`) opcode in the init code.
+        _store = new Store();
         // 0x6080604052348015600e575f80fd5b5060a58061001b5f395ff3fe6080604052348015600e575f80fd5b50600436106030575f3560e01c80632e64cec11460345780636057361d146048575b5f80fd5b5f5460405190815260200160405180910390f35b605760533660046059565b5f55565b005b5f602082840312156068575f80fd5b503591905056fea264697066735822122066c9c950dae1553a207fada55ceb3c8c4a2cbd99fa68605393a2cae9c2dab02364736f6c63430008150033
         console2.logBytes(type(Store).creationCode);
 
-        // These function interactions should fail due to the non-existent `PUSH0` (`5f`) opcode in the runtime code.
-        store.store(1);
-        assertEq(store.retrieve(), 1);
+        // These function interactions should actually fail due to the non-existent `PUSH0` (`5f`) opcode in the runtime code.
+        _store.store(1);
+        assertEq(_store.retrieve(), 1);
         // 0x6080604052348015600e575f80fd5b50600436106030575f3560e01c80632e64cec11460345780636057361d146048575b5f80fd5b5f5460405190815260200160405180910390f35b605760533660046059565b5f55565b005b5f602082840312156068575f80fd5b503591905056fea2646970667358221220a20cff41929e97651791862ac5880f8582f7ae69d846a86b70433f4db10c6a7464736f6c63430008150033
         console2.logBytes(type(Store).runtimeCode);
     }
